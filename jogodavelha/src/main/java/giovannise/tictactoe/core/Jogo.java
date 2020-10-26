45498070191b234afd929583b4e0a5392c2163a4
Giovannise <giovannise.lisboa@hotmail.com>
2020-10-26 17:51:52 -0300

    Detectando o próximo jogador

--- jogodavelha/src/main/java/giovannise/tictactoe/core/Jogo.java
+++ jogodavelha/src/main/java/giovannise/tictactoe/core/Jogo.java
@@ -7,6 +7,7 @@ public class Jogo {
 	
 	private Tabuleiro board = new Tabuleiro();
 	private Jogador[] players = new Jogador[Constantes.SYMBOL_PLAYER.length];
+	private int jogadorAtual = 0;
 	
 	public void play() {
 		UI.imprimeTituloDoJogo();
@@ -24,6 +25,27 @@ public class Jogo {
 		UI.imprimeTexto("O jogador " + name + " vai usar o símbolo '" + symbol +"'");
 		
 		return player;
+	}
+	
+	private Jogador proximo() {
+		jogadorAtual++;
+		
+		//quando chega no final da fila, recomeça a fila do 0
+		if(jogadorAtual >= players.length) {
+			jogadorAtual = 0;
+		}
+		
+		return players[jogadorAtual];
+		
+		/*
+		 * ---OUTRA OPÇÃO DE ABORDAGEM---
+		 * aqui ele vai pegar o valor atual do indice e dividir pelo tamanho do array
+		 * fazer teste de mesa para validar que sempre vai dar uma fila circular
+		 *  
+		 * jogadorAtual = (jogadorAtual +1) % players.length;
+		 * 
+		 * return players[jogadorAtual];
+		 */
 	}
 
 }
